64a70a097d39f694e8c3fa061c4f3c12eb7c17b9
Josh English <josh@joshenglish.com>
2016-12-22 21:44:25 -0600

    Updating so that everything transpiles and lints properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,22 @@
 var webpack = require('webpack'),
-    path    = require('path');
-
-var BUILD_DIR = path.resolve(__dirname, 'public/scripts');
-var APP_DIR   = path.resolve(__dirname, 'client/app');
+    path    = require('path'),
+    BUILD_DIR = path.resolve(__dirname, 'public/scripts'),
+    APP_DIR   = path.resolve(__dirname, 'client/app');
 
 var config = {
-    entry: APP_DIR + '/main.jsx',
-    output: {
-        path     : BUILD_DIR,
-        filename : 'bundle.js'
+  entry: APP_DIR + '/main.jsx',
+  output: {
+    path     : BUILD_DIR,
+      filename : 'bundle.js'
     },
     module : {
-        loaders : [
-            {
-                test    : /\.jsx?/,
-                include : APP_DIR,
-                loader  : 'babel'
-            }
-        ]
+      loaders : [
+        {
+          test    : /\.jsx?/,
+          include : APP_DIR,
+          loader  : 'babel'
+        }
+      ]
     }
 };
 
